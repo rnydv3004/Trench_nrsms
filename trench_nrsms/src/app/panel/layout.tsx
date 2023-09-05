998cf475454f42cbddd5d3dfadfd7c78a3aef2a4
Shiven <yaryan3087@gmail.com>
2023-09-05 10:23:03 +0530

    Firebse creteuser & auth page

--- trench_nrsms/src/app/panel/layout.tsx
+++ trench_nrsms/src/app/panel/layout.tsx
@@ -13,7 +13,6 @@ export default function DashboardLayout({
       <div className="h-full w-screen flex">
         <Sidenavcomponent />
         {children}
-        <Toaster/>
       </div>
     </section>
   );
